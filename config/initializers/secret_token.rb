8d32ca97bcc4d48787d0bda01c2e32a6abd60da5
KentWhite <Kent.White@hotmail.com>
2011-07-14 11:50:00 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeecaApp::Application.config.secret_token = '8ca4757079a89a9f51783a293b4c67bcf729f52f00bcdfc140b1683f1c8dbc268ce18cfc27c3328ad6d8c964a69fdf49c21a3d77ef30f1674bea0e700f58be5e'
